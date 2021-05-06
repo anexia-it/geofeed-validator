045cbc767e3ab050c3329500dfaecbdaa753f7fe
Raiko Wielk <RWielk@anexia-it.com>
2021-05-06 10:19:23 +0200

    Adjust setup.py to no longer support py2

--- setup.py
+++ setup.py
@@ -59,8 +59,6 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
